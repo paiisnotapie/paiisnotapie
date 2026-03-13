275dcef8e62b29a53ec2f8d83d08a7eaf0e91a46
Paiisnotapie <faizahalatsariyyah0@gmail.com>
2026-03-14 05:33:14 +0700

    Update app.js
    uncomment the js

--- js/app.js
+++ js/app.js
@@ -64,14 +64,3 @@ nameInput.addEventListener("keydown", (e) => {
   }
 
 })
-
-
-//     let j = 2;
-
-// for (let i = 0; i <= 10; i++) {
-//   if (j > 0) {
-//     console.log("ok");
-//   }
-
-//   j = 5 - (j * 2);
-// }
