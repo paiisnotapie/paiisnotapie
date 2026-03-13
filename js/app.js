7bce8b5d3a9fc5e8190dcc20f62a2fe04c44098a
paiisnotapie <faizahalatsariyyah0@gmail.com>
2026-03-14 04:56:47 +0700

    edit
    edit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// const editBtn = document.getElementById("editBtn")
+// const nameDisplay = document.getElementById("nameDisplay")
+// const nameInput = document.getElementById("nameInput")
+// const comma = document.getElementById("comma")
+
+// let editing = false
+
+
+// // Load name from Local Storage
+// const savedName = localStorage.getItem("username")
+
+// if(savedName){
+//   nameDisplay.textContent = savedName
+//   comma.textContent = ","
+// }
+
+
+// // Toggle edit mode
+// editBtn.addEventListener("click", () => {
+
+//   if(!editing){
+
+//     nameInput.style.display = "inline"
+//     nameDisplay.style.display = "none"
+
+//     nameInput.value = nameDisplay.textContent
+//     nameInput.focus()
+
+//     editing = true
+
+//   }else{
+
+//     nameInput.style.display = "none"
+//     nameDisplay.style.display = "inline"
+
+//     editing = false
+//   }
+
+// })
+
+
+// // Save name when pressing Enter
+// nameInput.addEventListener("keydown", (e) => {
+
+//   if(e.key === "Enter"){
+
+//     const name = nameInput.value.trim()
+
+//     if(name !== ""){
+//       nameDisplay.textContent = name
+//       comma.textContent = ","
+//       localStorage.setItem("username", name)
+
+//     }else{
+//       nameDisplay.textContent = ""
+//       comma.textContent = ""
+//       localStorage.removeItem("username")
+//     }
+
+//     nameInput.style.display = "none"
+//     nameDisplay.style.display = "inline"
+
+//     editing = false
+//   }
+
+// })
+
+
+    let j = 2;
+
+for (let i = 0; i <= 10; i++) {
+  if (j > 0) {
+    console.log("ok");
+  }
+
+  j = 5 - (j * 2);
+}
